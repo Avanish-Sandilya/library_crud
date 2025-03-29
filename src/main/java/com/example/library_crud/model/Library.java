6b9d50debfb66136a232451ba20eaf1921b0c0df
Avanish-Sandilya <bhanusandilya74@gmail.com>
2025-03-29 21:46:24 +0530

    adding Library model

---
+++ src/main/java/com/example/library_crud/model/Library.java
@@ -0,0 +1,58 @@
+package com.example.library_crud.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="library")
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookName;
+    private String author;
+    private int year;
+
+    public Library(){}
+
+    public Library(Long id, String bookName, String author, int year) {
+        this.id = id;
+        this.bookName = bookName;
+        this.author = author;
+        this.year = year;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}

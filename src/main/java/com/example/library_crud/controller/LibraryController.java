6cfc60591f795ce71827ab9d4eefd098a3308750
Avanish-Sandilya <bhanusandilya74@gmail.com>
2025-03-29 22:35:04 +0530

    adding controller layer

---
+++ src/main/java/com/example/library_crud/controller/LibraryController.java
@@ -0,0 +1,42 @@
+package com.example.library_crud.controller;
+import com.example.library_crud.model.Library;
+import com.example.library_crud.service.LibraryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/library")
+public class LibraryController {
+    @Autowired
+    private LibraryService libraryService;
+
+    @GetMapping
+    public List<Library> getBooks(@RequestParam String name){
+        return libraryService.getBooks(name);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Library> getBooksById(@PathVariable Long id){
+        return libraryService.getBookById(id);
+    }
+
+    @PostMapping
+    public Library addBook(@RequestBody Library library){
+        return libraryService.addBook(library);
+    }
+
+    @PutMapping("/{id}")
+    public Library updateBook(@PathVariable Long id, @RequestBody Library bookDetails){
+        return libraryService.updateBook(id,bookDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(Long id){
+        libraryService.deleteBook(id);
+    }
+
+
+}
